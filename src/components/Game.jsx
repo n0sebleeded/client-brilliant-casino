5f9499a78404099c710dae269f318294b72d3d42
n0sebleeded <devDanilAkhunzianov@outlook.com>
2023-08-02 03:01:11 +0400

    Merge remote-tracking branch 'origin/master'

---
+++ src/components/Game.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Box, Text} from "grommet";
+
+const Game = () => {
+    return (
+        <Box background="black" justify="center" align="center">
+            <Text className="time-text-play"> FUTURE GAME REALISATION</Text>
+        </Box>
+    );
+};
+
+export default Game;
