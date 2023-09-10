7923da4309c94ebeda0ad6b4953e814c45dc55a4
Danil Akhunzianov <devDanilAkhunzianov@outlook.com>
2023-09-11 01:14:33 +0300

    slots-game-upd

--- src/components/games/slots/slots.jsx
+++ src/components/games/slots/slots.jsx
@@ -1,7 +1,9 @@
 import React, {useState} from 'react';
+import {CSSTransition} from "react-transition-group";
 
 const Slots = () => {
 
+    const [count, setCount] = useState(0);
     const [slotState, setSlotState] = useState([
         {
             id: 1,
@@ -25,16 +27,21 @@ const Slots = () => {
             }
         })
         setSlotState(n);
+        setCount((prevState) => prevState+ 1);
     }
 
     return (
         <div className="slots-container">
-            <div className="slots-container-item">
-                {slotState.map((el, ind) => (
-                    <div key={ind} className="slots-item">{el.value}</div>
-                ))}
-            </div>
+                <div className="slots-container-item">
+                    {/* CSS TRANSITION!!*/}
+                        {slotState.map((el, ind) => (
+                            <div key={ind} className="slots-item">
+                                <p className="slots-el">{el.value}</p>
+                            </div>
+                        ))}
+                </div>
             <button onClick={handleSlot} className="roll-btn">roll</button>
+            <p>rolls count {count}</p>
         </div>
     );
 };
